d233aa6871e754989a0e88824cc31bd46f41af36
Bradley Fojas <bradfojas@gmail.com>
2024-05-27 17:12:51 -0700

    fix: experience title

--- src/utils/useData.js
+++ src/utils/useData.js
@@ -2,10 +2,10 @@ import data from './data.json';
 import codeFormatter from './codeFormatter';
 
 export default function useData() {
-  const { name, title, skills, links, experience: experiencedData, education } = data;
+  const { name, title, skills, links, experience: experienceData, education } = data;
   
   const about = codeFormatter({ name, title, skills, links });
-  const experience = codeFormatter({ experiencedData, education })
+  const experience = codeFormatter({ experience: experienceData, education })
 
   return { about, experience };
 }
